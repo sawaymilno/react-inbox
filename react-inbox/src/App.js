407aca0081ae89f7cd33215d114e03a62ed0834f
shawnmilne <shawn.m.milne@gmail.com>
2018-11-28 19:02:35 -0700

    setup and toolbar are working

---
+++ react-inbox/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import Toolbar from './Toolbar'
+class App extends Component {
+
+  state = {}
+
+  render() {
+
+    return (
+      <div className="App">
+        <Toolbar />
+      </div>
+    )
+  }
+}
+
+export default App;
